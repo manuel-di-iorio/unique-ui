3c01cf499d4b4e4652b5389526e5d940c8a213b2
manuel-di-iorio <manuel.diiorio.mail@gmail.com>
2026-05-29 12:16:54 +0200

    feat: add SVG to PNG converter with necessary configuration files

---
+++ other/convertSvgToPng/convert.js
@@ -0,0 +1,28 @@
+const sharp = require('sharp');
+const path = require('path');
+const fs = require('fs');
+const files = [
+  {
+    input: 'c:/Users/Manuel/GameMakerProjects/UniqueUI/sprites/sprUiIconToast/f37dd78c-639e-4bee-9dca-a771ebae9d4e.svg',
+    output: 'c:/Users/Manuel/GameMakerProjects/UniqueUI/sprites/sprUiIconToast/f37dd78c-639e-4bee-9dca-a771ebae9d4e.png'
+  }
+];
+async function convert() {
+  for (const file of files) {
+    try {
+      console.log(`Converting ${file.input} -> ${file.output}...`);
+      if (!fs.existsSync(file.input)) {
+        console.error(`Input file not found: ${file.input}`);
+        continue;
+      }
+      await sharp(file.input)
+        .resize(64, 64)
+        .png()
+        .toFile(file.output);
+      console.log(`Successfully generated: ${file.output}`);
+    } catch (err) {
+      console.error(`Error converting ${file.input}:`, err);
+    }
+  }
+}
+convert();
